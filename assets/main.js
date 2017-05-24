3c6583476de104f62e115215862609eb4b683764
MikeLDJ <mikeu76@gmail.com>
2017-05-24 17:31:38 +0200

    finished

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,26 @@
 $(function() {
 
-  // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/MikeLD.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      generateTempalate(response.courses.completed);
+      console.log(response.courses.completed);
+    }
+  });
 
+  function generateTempalate(courses) {
+    courses.forEach(function(course) {
+
+      const courseContainer = `<div class="course"></div>`;
+      const title = `<h3>${course.title}</h3>`;
+      const badge = `<img src="${course.badge}" />`;
+      const button = `<a href="${course.url}" class="btn btn-primary" target="_blank">See Course</a>`;
+
+      $('#badges').append(courseContainer);
+      $('.course:last-child').append(title).append(badge).append(button);
+
+    });
+
+  }
 });
